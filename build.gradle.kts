c75733c41f41279b2cf7f0cd260506a61098eb93
aleksseii <alesha.shkel@mail.ru>
2022-10-27 03:22:40 +0300

    moved all library dependencies in the root module, added new dependencies, removed repeatable pieces of code build.gradle.kts files

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,16 +6,39 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-
-    implementation("com.intellij:annotations:12.0")
+subprojects {
+    apply(plugin = "java")
 }
 
 allprojects {
+
     group = "ru.aleksseii"
     version = "1.0-SNAPSHOT"
+
+    dependencies {
+
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+        testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+        testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.1")
+
+        testImplementation("org.mockito:mockito-inline:4.8.1")
+        testImplementation("org.mockito:mockito-junit-jupiter:4.8.1")
+
+        testImplementation("org.hamcrest:hamcrest-all:1.3")
+
+        implementation("org.jetbrains:annotations:23.0.0")
+
+        compileOnly("org.projectlombok:lombok:1.18.24")
+        annotationProcessor("org.projectlombok:lombok:1.18.24")
+
+        implementation("com.google.code.gson:gson:2.10")
+    }
+
+    tasks {
+        test {
+            useJUnitPlatform()
+        }
+    }
 }
 
 tasks.getByName<Test>("test") {
